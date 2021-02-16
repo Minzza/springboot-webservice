e3e44f2b82eeee47afb78bb701fea9cb4ee43761
Minzza <cmjasdf@gmail.com>
2021-02-17 00:39:57 +0900

    3장 JpaRepository 테스트

---
+++ src/main/java/com/minzza/admin/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.minzza.admin.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
